505d02074022d4844d5bfd47195b023c46abfb6c
Florian Witteler <florian.witteler@hivemindtechnologies.com>
2020-03-21 13:35:30 +0100

    Fixes #2. fixed elasticsearch to run in vpc (needed to be recreated).

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -10,14 +10,15 @@ export class InfrastructureStack extends cdk.Stack {
       cidr: '178.22.0.0/20',
     });
 
+
     const privateSubnetIds = vpc.privateSubnets.map((subnet) => subnet.subnetId);
     const defaultSg = ec2.SecurityGroup.fromSecurityGroupId(this, 'default-sg', vpc.vpcDefaultSecurityGroup);
 
     // The code that defines your stack goes here
 
-    const elasticSearch = new es.CfnDomain(this, "covid-elasticsearch-vpc",
+    const elasticSearch = new es.CfnDomain(this, "covid-es-in-vpc",
       {
-        domainName: 'covid-elasticsearch',
+        domainName: 'covid-es-in-vpc',
         elasticsearchVersion: '6.0',
         elasticsearchClusterConfig: {
           instanceCount: 1,
@@ -30,51 +31,10 @@ export class InfrastructureStack extends cdk.Stack {
         },
         vpcOptions: {
           securityGroupIds: [defaultSg.securityGroupId],
-          subnetIds: privateSubnetIds
+          subnetIds: [privateSubnetIds[0]] //although it's an array, it requires only one subnet
         }
-        //FIXME: restrict access
-        // accessPolicies: {
-        //   "Version": "2012-10-17",
-        //   "Statement": [
-        //     {
-        //       "Effect": "Allow",
-        //       "Principal": { "AWS": "*" },
-        //       "Action": ["es:*"],
-
-        //     }]
-        // }
       }
     );
   }
 }
 
-/*
-aws es update-elasticsearch-domain-config --domain-name mylogs --access-policies '
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::123456789012:root"
-      },
-      "Action":"es:*",
-      "Resource":"arn:aws:es:us-east-1:555555555555:domain/index1/*"
-    }
-  ] }'
-*/
-
-/*
-
-aws es create-elasticsearch-domain
-
---domain-name movies
- --elasticsearch-version 6.0
- --elasticsearch-cluster-config
-   InstanceType=t2.small.elasticsearch,
-   InstanceCount=1
- --ebs-options EBSEnabled=true,VolumeType=standard,VolumeSize=10
- --access-policies '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"AWS":"*"},"Action":["es:*"],"Condition":{"IpAddress":{"aws:SourceIp":["your_ip_address"]}}}]}'
-
-
- */
