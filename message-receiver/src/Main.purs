614d4772d4b3ad88c2a9996e1d6b35f31264d31a
Florian Witteler <florian.witteler@hivemindtechnologies.com>
2020-03-21 20:47:22 +0100

    added post to elasticsearch. Doesn't work yet.
    It seems like the `launchAff_` doesn't start before the lambda stops

--- message-receiver/src/Main.purs
+++ message-receiver/src/Main.purs
@@ -1,14 +1,12 @@
 module Main where
 
 import Prelude
-
 import Data.Either (Either(..))
 import Data.Semigroup.Foldable (intercalateMap)
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
 import Effect.Class.Console (log)
 import Foreign (renderForeignError)
-
 import Foreign.Object as FO
 import Milkis as M
 import Milkis.Impl.Node (nodeFetch)
@@ -19,10 +17,10 @@ type Input
     }
 
 type Output
-  = { statusCode :: Int,
-      body :: String,
-      isBase64Encoded :: Boolean,
-      headers :: FO.Object String
+  = { statusCode :: Int
+    , body :: String
+    , isBase64Encoded :: Boolean
+    , headers :: FO.Object String
     }
 
 type Location
@@ -49,20 +47,17 @@ fetch = M.fetch nodeFetch
 fetchGoogle :: Aff M.Response
 fetchGoogle = fetch (M.URL "https://www.google.com") M.defaultFetchOptions
 
+postElasticSearch :: String -> Aff M.Response
+postElasticSearch body = fetch (M.URL "https://vpc-covid-es-in-vpc-sattub32j5fqdokoslmc4kjvvi.eu-west-1.es.amazonaws.com/vouchers/voucherOffer/") opts
+  where
+  opts =
+    { method: M.postMethod
+    , body: body
+    , headers: M.makeHeaders { "Content-Type": "application/json" }
+    }
+
 logVoucherOffer :: VoucherOffer -> Aff Unit
 logVoucherOffer vo = log (writeJSON vo)
-
-main :: Effect Unit
-main =
-  launchAff_ do
-    let
-      voucherOffer = readJSON example
-    case voucherOffer of
-      Left errors -> log (intercalateMap "\n" renderForeignError errors)
-      Right ok -> logVoucherOffer ok
-    response <- fetchGoogle
-    text <- M.text response
-    log text
 
 responseHeaders :: FO.Object String
 responseHeaders = FO.empty
@@ -70,10 +65,16 @@ responseHeaders = FO.empty
 run :: Input -> Effect Output
 run { body } = do
   log $ "Received body " <> body
-  pure $ { statusCode : 200, body: responseBody, isBase64Encoded : false , headers : responseHeaders }
+  launchAff_ do
+    let
+      voucherOffer = readJSON example
+    case voucherOffer of
+      Left errors -> log (intercalateMap "\n" renderForeignError errors)
+      Right ok -> logVoucherOffer ok
+    response <- postElasticSearch body
+    text <- M.text response
+    log $ "Received this from elasticsearch " <> text
+  pure $ { statusCode: 200, body: responseBody, isBase64Encoded: false, headers: responseHeaders }
   where
-  responseBody = writeJSON { hello: "World" } 
-  
--- handler :: Input -> Output
--- handler = unsafePerformEffect <<< run
-
+  responseBody = writeJSON { hello: "World" }
+ -- handler :: Input -> Output -- handler = unsafePerformEffect <<< run
