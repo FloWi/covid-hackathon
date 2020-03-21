f46a80d22e1930d8cc7a33d6062cb845efa1a482
realvictorprm <mueller.vpr@gmail.com>
2020-03-21 18:16:46 +0000

    Add seperate security group for elastic search & add open access policies such that access from bastion is possible

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as es from "@aws-cdk/aws-elasticsearch";
 import * as ec2 from '@aws-cdk/aws-ec2';
+import * as iam from "@aws-cdk/aws-iam"
 import { BastionHostLinux, Peer } from "@aws-cdk/aws-ec2";
 
 
@@ -16,8 +17,16 @@ export class InfrastructureStack extends cdk.Stack {
     const privateSubnetIds = vpc.privateSubnets.map((subnet) => subnet.subnetId);
     const defaultSg = ec2.SecurityGroup.fromSecurityGroupId(this, 'default-sg', vpc.vpcDefaultSecurityGroup);
 
-    // The code that defines your stack goes here
+    const sg = new ec2.SecurityGroup(this, 'elastic-search', {
+      vpc,
+      description: "allow access to es",
+      allowAllOutbound: true
+    })
 
+    sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.allTraffic(), "allow all traffic")
+
+    // The code that defines your stack goes here
+    const securityGroupIds = [defaultSg, sg].map((sg) => sg.securityGroupId)
     const elasticSearch = new es.CfnDomain(this, "covid-es-in-vpc",
       {
         domainName: 'covid-es-in-vpc',
@@ -32,8 +41,23 @@ export class InfrastructureStack extends cdk.Stack {
           volumeSize: 10
         },
         vpcOptions: {
-          securityGroupIds: [defaultSg.securityGroupId],
+          securityGroupIds: securityGroupIds,
           subnetIds: [privateSubnetIds[0]] //although it's an array, it requires only one subnet
+        },
+        accessPolicies: {
+          "Version": "2012-10-17",
+          "Statement": [
+            {
+              "Effect": "Allow",
+              "Principal": {
+                "AWS": "*"
+              },
+              "Action": [
+                "es:*"
+              ],
+              "Resource": "*"
+            }
+          ]
         }
       }
     )
